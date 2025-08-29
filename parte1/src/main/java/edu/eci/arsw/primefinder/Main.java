1683d6194013d975017ab53d38c04fa542d4dbee
Diego Cardenas <diegosp778@gmail.com>
2025-08-29 16:26:12 -0500

    feat: implements the task for part 1 - In this commit i implement the difenten coents - docs: document the class and methods

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -3,12 +3,11 @@ package edu.eci.arsw.primefinder;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 50000000);
-		
+
+		PrimeFinderThread pft = new PrimeFinderThread(0, 50000000);
+
 		pft.start();
-		
-		
+
 	}
-	
+
 }
--- parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -3,44 +3,82 @@ package edu.eci.arsw.primefinder;
 import java.util.LinkedList;
 import java.util.List;
 
-public class PrimeFinderThread extends Thread{
+/**
+ * PrimeFinderThread is a thread that finds all prime numbers within a given
+ * range [a, b].
+ * It stores the found primes in a list and prints each prime to the standard
+ * output.
+ *
+ * Usage:
+ * - Instantiate with the desired range.
+ * - Start the thread to begin prime searching.
+ * - Retrieve the list of found primes using {@link #getPrimes()}.
+ *
+ * Note:
+ * - The primality test used is optimized for odd numbers greater than 2.
+ * - The thread is not thread-safe for concurrent access to the primes list.
+ * 
+ * @author Diego Cardenas
+ * 
+ */
+public class PrimeFinderThread extends Thread {
 
-	
-	int a,b;
-	
-	private List<Integer> primes=new LinkedList<Integer>();
-	
+	int a, b;
+
+	private List<Integer> primes = new LinkedList<Integer>();
+
+	/**
+	 * Constructs a PrimeFinderThread with the specified range.
+	 *
+	 * @param a the starting integer of the range (inclusive)
+	 * @param b the ending integer of the range (inclusive)
+	 */
 	public PrimeFinderThread(int a, int b) {
 		super();
 		this.a = a;
 		this.b = b;
 	}
 
-	public void run(){
-		for (int i=a;i<=b;i++){						
-			if (isPrime(i)){
+	/**
+	 * Executes the prime number search within the specified range [a, b].
+	 * Iterates through each integer in the range, checks if it is prime using
+	 * {@code isPrime(i)},
+	 * and if so, adds it to the {@code primes} collection and prints the prime
+	 * number to the console.
+	 * This method is intended to be run in a separate thread.
+	 */
+	@Override
+	public void run() {
+		for (int i = a; i <= b; i++) {
+			if (isPrime(i)) {
 				primes.add(i);
 				System.out.println(i);
 			}
 		}
-		
-		
-	}
-	
-	boolean isPrime(int n) {
-	    if (n%2==0) return false;
-	    for(int i=3;i*i<=n;i+=2) {
-	        if(n%i==0)
-	            return false;
-	    }
-	    return true;
 	}
 
+	/**
+	 * Determines whether a given integer is a prime number.
+	 *
+	 * @param n the integer to check for primality
+	 * @return true if {@code n} is a prime number, false otherwise
+	 */
+	boolean isPrime(int n) {
+		if (n % 2 == 0)
+			return false;
+		for (int i = 3; i * i <= n; i += 2) {
+			if (n % i == 0)
+				return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Returns the list of prime numbers found by this thread.
+	 *
+	 * @return a list of integers representing the prime numbers.
+	 */
 	public List<Integer> getPrimes() {
 		return primes;
 	}
-	
-	
-	
-	
 }
